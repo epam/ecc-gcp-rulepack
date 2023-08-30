33cf0ef00408803e2c282ca67ab5efa39469d80b
Vitalii Kanivets <vitalii_kanivets@epam.com>
2023-08-30 09:08:01 +0000

    skip: new policy ecc-gcp-445-disk_with_one_snapshot

---
+++ terraform/ecc-gcp-445-disk_with_one_snapshot/green/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "snapshot_name" {
+  type = string
+}
+
+variable "disk_name" {
+  type = string
+}
