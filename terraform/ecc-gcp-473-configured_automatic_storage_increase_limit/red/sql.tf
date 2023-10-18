3bc933b4691edcfd7e6f1199437d6b1c528197fa
Vitalii Kanivets <vitalii_kanivets@epam.com>
2023-10-18 10:16:28 +0000

    new: added policy ecc-gcp-473-configured_automatic_storage_increase_limit

---
+++ terraform/ecc-gcp-473-configured_automatic_storage_increase_limit/red/sql.tf
@@ -0,0 +1,18 @@
+resource "random_id" "this" {
+  byte_length = 4
+}
+
+resource "google_sql_database_instance" "this" {
+  name                = "${var.dbname}-${random_id.this.hex}-473-red"
+  database_version    = var.dbver
+  region              = var.region
+  deletion_protection = false
+
+  settings {
+    user_labels = {
+      custodiarule     = "ecc-gcp-473-configured_automatic_storage_increase_limit"
+      compliancestatus = "red"
+    }
+    tier = var.tier
+  }
+}
