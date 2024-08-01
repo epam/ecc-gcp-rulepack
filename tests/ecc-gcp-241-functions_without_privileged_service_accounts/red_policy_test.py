0992f5cd0789e8c7c38d6af4b6f4daf56c77cf08
jamthehackerscat <andronnikova.anna@gmail.com>
2024-08-01 13:03:49 +0300

    new: add policy ecc-gcp-241-functions_without_privileged_service_accounts

---
+++ tests/ecc-gcp-241-functions_without_privileged_service_accounts/red_policy_test.py
@@ -0,0 +1,6 @@
+class PolicyTest(object):
+
+    def test_resources(self, base_test, resources):
+        base_test.assertEqual(len(resources), 1)
+        base_test.assertIn("roles/editor", resources[0]['c7n:iamPolicy']['bindings'][0]['role'])
+        base_test.assertRegex(";".join(resources[0]['c7n:iamPolicy']['bindings'][0]['members']), r'.*gserviceaccount.com.*')
